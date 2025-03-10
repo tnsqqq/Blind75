14d6c84f23c183715968f39fcc669e99c2e619d8
tnsqqq <tnsqqqbaliyan@gmail.com>
2025-03-10 21:24:07 +0530

    bit

--- Binary/ReverseBits.java
+++ Binary/ReverseBits.java
@@ -6,7 +6,7 @@ public class ReverseBits {
 
   static int reverseBits(int n) {
     int result = 0;
-    for (int i = 1; i < 32; i++) {
+    for (int i = 0; i < 32; i++) {
       int lsb = n & 1;
       result = (result << 1) | lsb;
       n = n >> 1;
